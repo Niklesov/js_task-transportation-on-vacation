92f28e01b7edad5541ca4c6f489122512ade7e0c
Niklesov <n1klesov1@gmail.com>
2025-03-04 16:49:42 +0200

    ALL DONE

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,29 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
 function calculateRentalCost(days) {
-  // write code here
+  let sum = 0;
+
+  if (days < 3) {
+    for (let i = 0; i < days; i++) {
+      sum += 40;
+    }
+
+    return sum;
+  }
+
+  if (days < 7) {
+    for (let i = 0; i < days; i++) {
+      sum += 40;
+    }
+
+    return sum - 20;
+  }
+
+  if (days >= 7) {
+    for (let i = 0; i < days; i++) {
+      sum += 40;
+    }
+
+    return sum - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
